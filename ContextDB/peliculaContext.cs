561f9da75f2197e28fa2be804fd05b2fd8aff54c
ing Wilnel <perezwilnel12@gmail.com>
2025-02-18 00:49:35 -0400

    creacion del contexto a la base de datos y el modelo de la tabla

---
+++ ContextDB/peliculaContext.cs
@@ -0,0 +1,12 @@
+﻿using AdministrarPeliculas_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdministrarPeliculas_API.ContextDB
+{
+    public class peliculaContext : DbContext
+    {
+        public peliculaContext (DbContextOptions<peliculaContext> db ) : base(db) { }
+
+        public DbSet<pelicula> pelicula { get; set; }
+    }
+}
---
+++ Models/pelicula.cs
@@ -0,0 +1,11 @@
+﻿namespace AdministrarPeliculas_API.Models
+{
+    public class pelicula
+    {
+        public int Id { get; set; }
+        public string titulo { get; set; }
+        public string genero { get; set; }
+        public string autor { get; set; }
+        public string anio { get; set; }
+    }
+}
